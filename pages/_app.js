6a403e91309afdae0990593950937644944858c0
João Vitor de Sá Melo <joaovitormelo199@gmail.com>
2024-05-07 16:13:35 -0300

    final-changes

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,23 @@ export default function MyApp({ Component, pageProps }) {
     return(
      <>
          <Head>
+                                          {/*   <!-- Google tag (gtag.js) --> */}
+
+                        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3BHSEMMZQ7"></script>
+                        <script 
+                           dangerouslySetInnerHTML={{
+                              __html: `
+                              window.dataLayer = window.dataLayer || [];
+                              function gtag(){dataLayer.push(arguments);}
+                              gtag('js', new Date());
+      
+                              gtag('config', 'G-3BHSEMMZQ7');
+                               
+                              `
+                           }}
+                        
+                        />
+                       
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
             <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
--- pages/faq.js
+++ pages/faq.js
@@ -2,6 +2,10 @@ import Link from '../src/components/Link';
 import { useEffect, useState } from 'react';
 import Head from 'next/head'
 
+import FAQScreen from '../src/components/screens/FAQScreen';
+
+export default FAQScreen;
+
 export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
     const faq = await fetch(FAQ_API_URL)
@@ -21,32 +25,3 @@ export async function getStaticProps() {
      };
   }
    
-
-
-
-export default function FAQPage ({ faq }) {
-    return (
-        <div>
-        <Head>
-            <title>FAQ - Alura Cases Campanha</title>
-        </Head>
-            <h1>Página do FAQ</h1>
-            <Link href="/" legacyBehavior>
-                Ir para a home
-            </Link>
-            <ul>
-                {faq.map(({ answer, question}) => (
-                    <li key={ question }>
-                    <article>
-
-                        <h2>{ question }</h2>
-                        <p>{ answer }</p>
-
-                    </article>
-                    </li>
-                ))}
-            </ul>
-
-        </div>
-    )
-}
